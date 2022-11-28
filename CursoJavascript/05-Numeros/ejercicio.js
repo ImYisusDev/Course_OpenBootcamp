bb7d72373a791cbf82ff1719aea2fec1f7263866
Jesus <rojasalarcone@gmail.com>
2022-11-27 20:20:05 -0500

    Números en Javascript - Curso Javascript

---
+++ CursoJavascript/05-Numeros/ejercicio.js
@@ -0,0 +1,10 @@
+let alturaInt = Number.parseInt(170);
+let alturaFloat = Number.parseFloat(1.70);
+
+let pesoKg = Number.parseFloat(50.95);
+
+let alturaRedondeada = alturaFloat.toFixed(3);
+
+let pesoRedondeado = pesoKg.toFixed(1);
+
+let isMaxMasUnoIgualAMax = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
