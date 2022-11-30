db69c1ff1581f8e35d8e7b6b5756b7016af1c2f9
Jesus <rojasalarcone@gmail.com>
2022-11-29 21:46:40 -0500

    ESLint - Curso Javascript

---
+++ CursoJavascript/13-ESLint/eslint-npm/index.js
@@ -0,0 +1,25 @@
+// Este es el archivo que vamos a utilizar
+
+const nombre = "Jesús"
+
+
+/* eslint-disable */
+
+const persona2 = 'María'
+
+/* eslint-enable */
+
+
+//? En esta línea quiero tener comillas simples (quiero desactivar la regla de las comillas dobles)
+const nuevoString = 'Este es un nuevo string'; // eslint-disable-line
+
+/* eslint-disable-next-line indent */
+  const string2 = "Más strings"
+
+console.log("hola")
+
+const persona3 = "Juan"
+
+const nombre4 = "Julián"
+
+console.log(4)
---
+++ CursoJavascript/13-ESLint/index.js
@@ -0,0 +1,10 @@
+const nombre = "Jesús"
+
+var nombre2 = 'María';
+
+var objeto = {
+  nombre: 'circulo',
+  radio: 2,
+  nombre: "rectángulo"
+}
+
