6bd99b0b3d31ac25d5a2176b34209756c7f924f0
Jesus <rojasalarcone@gmail.com>
2022-12-04 12:58:29 -0500

    Componentes y jerarquía - Curso React

--- CursoReactJs/hola-mundo/src/App.js
+++ CursoReactJs/hola-mundo/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/pure/greeting';
+import GreetingF from './components/pure/greetingF';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HOLA MUNDO!
-        </p>
+        {/* Componente propio Greeting.jsx */}
+        {/* <Greeting name={"Martin"}></Greeting> */}
+        <GreetingF name={"Martin"}></GreetingF>
       </header>
     </div>
   );
